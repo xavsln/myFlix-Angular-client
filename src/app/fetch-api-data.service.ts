c0b1f64883ef771bd4571dfcddf7d317a89b547b
xavsln <19362582+xavsln@users.noreply.github.com>
2022-11-04 18:30:57 +0100

    Initial commit

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+
+// import { catchError } from 'rxjs/internal/operators'; // Proposed code from course shows an error
+import { catchError } from 'rxjs/operators';
+
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+//Declaring the api url that will provide data for the client app
+const apiUrl = 'https://themyflixapp.herokuapp.com/';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FetchApiDataService {
+  // Inject the HttpClient module to the constructor params
+  // This will provide HttpClient to the entire class, making it available via this.http
+  constructor(private http: HttpClient) {}
+
+  // Making the api call for the user registration endpoint
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http
+      .post(apiUrl + 'users', userDetails)
+      .pipe(catchError(this.handleError));
+  }
+
+  // -----------------------------------------------
+  // Making the api call for the user login endpoint
+  // -----------------------------------------------
+  public userLogin(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http
+      .post(apiUrl + 'login', userDetails)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` + `Error body is: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
+
+  // -----------------------------------------------
+  // Making the api call for the All Movies endpoint
+  // -----------------------------------------------
+  getAllMovies(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http
+      .get(apiUrl + 'movies', {
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + token,
+        }),
+      })
+      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  }
+  // Non-typed response extraction
+  private extractResponseData(res: Response): any {
+    const body = res;
+    return body || {};
+  }
+}
